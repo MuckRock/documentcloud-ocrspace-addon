5c0f9eaea318d216f021624c7f6e74e12a5199c6
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-10-12 12:41:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,9 @@ class OCRSpace(AddOn):
                 pages.append(page)
             resp = self.client.patch(f"documents/{document.id}/", json={"pages": pages})
             resp.raise_for_status()
-        if errors > 0:    
-            self.set_message(f"Skipped {errors} files because they were too big")     
+        if errors > 0:
+            efiles = "file" if errors == 1 else "files"
+            self.set_message(f"Skipped  {errors} {efiles} because they were too big")     
 
 if __name__ == "__main__":
     OCRSpace().main()
